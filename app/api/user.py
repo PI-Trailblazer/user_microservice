12adaf669c15c82f52de36c6e9958170f675c260
André Almeida Oliveira <andreaoliveira@ua.pt>
2024-03-18 15:01:33 +0000

    TRAIL-17 start register endpoint

--- app/api/user.py
+++ app/api/user.py
@@ -1,9 +1,27 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from firebase_admin import auth
+from typing import List, Optional
+
+""" from app.api.deps import get_db """
 
 router = APIRouter()
 
-# This is an example delete whatever you deem necessary
-@router.get("/example")
-async def read_item():
-    return {"message": "Hello World"}
 
+class RegisterData(BaseModel):
+    roles: List[str]
+    phone_number: Optional[str] = None
+
+
+class AuthHeader(BaseModel):
+    Authorization: str
+
+
+@router.post("/api/register")
+async def register_endpoint(*, user_in: RegisterData, headers: AuthHeader = Depends()):
+    # Now you can access the data sent by the `register` function
+    # data.roles, data.phone_number, headers.Authorization
+    # Write your logic here
+    print(user_in)
+    print(headers)
+    pass
